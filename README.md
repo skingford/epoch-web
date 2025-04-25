e7f85636d390d5d303241d2af8e160a0a12cbee3
zjf <zhongfjinfu@moren.com>
2025-04-25 18:42:55 +0800

    feat: vite plugin

--- README.md
+++ README.md
@@ -5,4 +5,24 @@
 ```bash
 # 创建项目
 npm create vite@latest epoch-web -- --template vue-ts
+
+pnpm add @vitejs/plugin-legacy -D
+
+pnpm add vite-plugin-inspect -D
+
+pnpm add unplugin-auto-import -D
+
+pnpm add vite-plugin-restart -D
+
+pnpm add vite-plugin-svg-icons -D
+
+pnpm add vite-plugin-compression -D
+
+pnpm add  vite-plugin-cdn-import -D
+
+pnpm add vite-plugin-dts -D
+
+pnpm add rollup-plugin-visualizer -D
+
+pnpm add vite-plugin-vconsole -D
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import legacy from '@vitejs/plugin-legacy'
+import { viteVConsole } from 'vite-plugin-vconsole';
+import {resolve} from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia']
+    }),
+    viteVConsole({
+      entry: resolve('src/main.ts'), 
+      enabled: true,
+      config: {
+        log: {
+          maxLogNumber: 1000,
+        },
+        theme: 'light', // light | dark
+      }
+    })
+  ],
+  server: {
+    host: true, // 允许局域网访问
+    port: 8080
+  },
 })
