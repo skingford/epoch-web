9350c088b907b1c0d1af13d84a6dc9d359544cdc
SteveKingFord <332312568@qq.com>
2025-04-26 08:07:48 +0800

    "不规范的提交消息"

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,8 +4,18 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']
+      [
+        'feat',     // 新功能
+        'fix',      // 修复 bug
+        'docs',     // 文档变更
+        'style',    // 代码格式调整
+        'refactor', // 代码重构
+        'test',     // 测试相关
+        'chore',    // 构建或辅助工具变更
+        'revert',   // 回退代码
+        'perf'      // 性能优化
+      ]
     ],
-    'subject-case': [0, 'never', ['lower-case']]
+    'subject-case': [0] // 不限制 subject 大小写
   }
-}
+};
