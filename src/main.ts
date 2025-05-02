cea885d3900c9176d132dd6168693cbe9a5271f8
SteveKingFord <332312568@qq.com>
2025-05-02 20:16:55 +0800

    chore(version): version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
-import { createApp } from 'vue'
-import 'virtual:svg-icons-register'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "virtual:svg-icons-register";
+import "./style.css";
+import App from "./App.vue";
+import { createVersionPolling } from "version-polling";
 
-createApp(App).mount('#app')
+function bootstrap() {
+  const app = createApp(App);
+
+  createVersionPolling({
+    vcType: "chunkHash",
+    chunkName: "index",
+    silent: false,
+    onUpdate: (self) => {
+      const result = confirm("页面有更新，点击确定刷新页面！");
+      if (result) {
+        self.onRefresh();
+      } else {
+        self.onCancel();
+      }
+    },
+  });
+
+  app.mount("#app");
+}
+
+bootstrap();
