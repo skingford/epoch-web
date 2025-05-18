8e8391970f27bc15429bd8ad66ab8ddda0770099
SteveKingFord <332312568@qq.com>
2025-05-18 16:36:37 +0800

    feat: nav

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<el-config-provider :size="size" :z-index="zIndex">
+  <el-config-provider :size="size" :z-index="zIndex">
     <router-view v-slot="{ Component }">
       <transition>
         <keep-alive :max="10" :include="keeps">
@@ -15,7 +15,7 @@ import { buildKeepAliveRoutes, router } from "@/routes";
 
 console.log('[ Version ] >', Version());
 
-const size = ref<'default' | 'small' | 'large'>('small')
+const size = ref<'default' | 'small' | 'large'>('default')
 const zIndex = ref(3000)
 
 const routes = router.getRoutes()
--- apps/web/src/pages/layout/LayoutNav.vue
+++ apps/web/src/pages/layout/LayoutNav.vue
@@ -5,10 +5,43 @@
       <!-- <i-ep-expand /> -->
     </el-icon>
     <div class="layout-nav-content">
-      content
+      <div class="layout-nav-content-item">
+        <el-icon size="24" class="nav-icon">
+          <i-ep-refresh />
+        </el-icon>
+      </div>
+      <div class="layout-nav-content-item">
+        <el-icon size="24" class="nav-icon">
+          <i-ep-full-screen />
+        </el-icon>
+      </div>
+      <div class="layout-nav-content-item">
+        <el-dropdown trigger="click">
+          <span class="el-dropdown-link">
+            <span>admin</span>
+            <el-icon class="el-icon--right"><arrow-down /></el-icon>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item :icon="Plus">个人中心</el-dropdown-item>
+              <el-dropdown-item :icon="CirclePlusFilled">
+                退出
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
     </div>
   </header>
 </template>
+<script lang="ts" setup>
+import {
+  ArrowDown,
+  CirclePlusFilled,
+  Plus,
+
+} from '@element-plus/icons-vue'
+</script>
 <style lang="scss" scoped>
 .layout-nav {
   height: 55px;
@@ -26,6 +59,20 @@
     &:hover {
       color: var(--el-color-primary);
       transform: scale(1.1);
+    }
+  }
+
+  &-content {
+    display: flex;
+    align-items: center;
+    height: 100%;
+
+    &-item {
+      display: flex;
+      align-items: center;
+      padding: 0 10px;
+      height: 100%;
+      cursor: pointer;
     }
   }
 }
